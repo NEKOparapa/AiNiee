c2f9cde45c6f5203c7fdde920d939a270d2a58a8
neavo <neavo@neavo.me>
2025-01-23 01:31:59 +0800

    调整 - 文本规范实现逻辑优化 (#405)

--- Plugin_Scripts/TextNormalizer/TextNormalizer.py
+++ Plugin_Scripts/TextNormalizer/TextNormalizer.py
@@ -1,8 +1,6 @@
-import jaconv
-import unicodedata
-
 from Plugin_Scripts.PluginBase import PluginBase
 from Module_Folders.Translator.TranslatorConfig import TranslatorConfig
+from Plugin_Scripts.TextNormalizer.Normalizer import Normalizer
 
 class TextNormalizer(PluginBase):
 
@@ -27,23 +25,5 @@ class TextNormalizer(PluginBase):
 
     # 文本规范化事件
     def on_normalize_text(self, event: str, config: TranslatorConfig, data: dict) -> None:
-        if config.source_language == "英语":
-            for k in data.keys():
-                data[k] = unicodedata.normalize("NFKC", data.get(k, ""))
-        elif config.source_language == "日语":
-            for k in data.keys():
-                # Convert Half-width (Hankaku) Katakana to Full-width (Zenkaku) Katakana
-                # kana (bool) – Either converting Kana or not.
-                # ascii (bool) – Either converting ascii or not.
-                # digit (bool) – Either converting digit or not.
-                text = jaconv.hankaku2zenkaku(data.get(k, ""), kana=True, ascii=False, digit=False)
-
-                # Convert Full-width (Zenkaku) Katakana to Half-width (Hankaku) Katakana
-                # kana (bool) – Either converting Kana or not.
-                # ascii (bool) – Either converting ascii or not.
-                # digit (bool) – Either converting digit or not.
-                text = jaconv.zenkaku2hankaku(text, kana=False, ascii=True, digit=True)
-
-                # 注意，不再直接使用 jaconv.normalize 方法，以避免部分符号被错误的转换
-                # https://github.com/ikegami-yukino/jaconv?tab=readme-ov-file
-                data[k] = text
+        for k in data.keys():
+            data[k] = Normalizer.normalize(data.get(k, ""), merge_space = False)
