92e668b0e1e8b691d1c4248251d840998cd9ad6f
lastsummerx <lastsummerx@163.com>
2025-04-29 19:33:56 +0800

    调整缓存结构

---
+++ ModuleFolders/Cache/CacheFile.py
@@ -0,0 +1,54 @@
+import os
+from dataclasses import dataclass, field
+from functools import cached_property
+from typing import Any
+
+from ModuleFolders.Cache.BaseCache import ExtraMixin, ThreadSafeCache
+from ModuleFolders.Cache.CacheItem import CacheItem
+
+
+@dataclass(repr=False)
+class CacheFile(ThreadSafeCache, ExtraMixin):
+    """文件元数据"""
+
+    storage_path: str = ""
+    """文件相对路径"""
+
+    encoding: str = "utf-8"
+    """默认编码"""
+
+    file_project_type: str = ""
+    """文件项目类型"""
+
+    line_ending: str = "\n"
+    """默认换行符"""
+
+    items: list[CacheItem] = field(default_factory=list)
+    """原文片段列表"""
+
+    extra: dict[str, Any] = field(default_factory=dict)
+    """额外属性，用于存储特定reader产生的文件的额外属性，共用属性请加到CacheFile中"""
+
+    @property
+    def file_name(self):
+        return os.path.split(self.storage_path)[1]
+
+    def add_item(self, item: CacheItem) -> None:
+        """线程安全添加缓存项"""
+        with self._lock:
+            if hasattr(self, "items_dict"):
+                del self.items_dict
+            self.items.append(item)
+
+    def get_item(self, text_index: int) -> CacheItem:
+        """线程安全获取缓存项"""
+        with self._lock:
+            return self.items[self.items_index_dict[text_index]]
+
+    @cached_property
+    def items_index_dict(self):
+        """item的全局唯一id对应当前file items里的下标"""
+        return {v.text_index: i for i, v in enumerate(self.items)}
+
+    def index_of(self, text_index):
+        return self.items_index_dict[text_index]
--- ModuleFolders/Cache/CacheItem.py
+++ ModuleFolders/Cache/CacheItem.py
@@ -1,6 +1,11 @@
-import threading
+from dataclasses import dataclass, field
+from functools import cache
+from typing import Any, ClassVar
 
 import tiktoken
+
+from ModuleFolders.Cache.BaseCache import ExtraMixin, ThreadSafeCache
+
 
 class Status():
 
@@ -9,146 +14,29 @@ class Status():
     TRANSLATING = 2         # 翻译中（弃用）
     EXCLUED = 7             # 已排除
 
-class CacheItem():
 
-    STATUS = Status()
-    TYPE_FILTER = (int, str, bool, float, list, dict, tuple)
+@dataclass(repr=False)
+class CacheItem(ThreadSafeCache, ExtraMixin):
+    text_index: int = 0
+    translation_status: int = 0
+    model: str = ''
+    source_text: str = ''
+    translated_text: str = None
+    extra: dict[str, Any] = field(default_factory=dict)
+    """额外属性，用于存储特定reader产生的原文片段的额外属性，共用属性请加到CacheItem中"""
 
-    def __init__(self, args: dict) -> None:
-        super().__init__()
+    def __post_init__(self):
+        if self.translated_text is None:
+            self.translated_text = self.source_text or ""
+        super().__post_init__()
 
-        # 默认值
-        self.row_index: int = 0
-        self.text_index: int = 0
-        self.translation_status: int = 0
-        self.model: str = ""
-        self.source_text: str = ""
-        self.translated_text: str = ""
-        self.file_name: str = ""
-        self.storage_path: str = ""
-        self.file_project_type: str = ""
+    @property
+    def token_count(self):
+        return self.get_token_count(self.source_text)
 
-        # 初始化
-        for k, v in args.items():
-            setattr(self, k, v)
+    @classmethod
+    @cache
+    def get_token_count(cls, text) -> int:
+        return len(tiktoken.get_encoding("cl100k_base").encode(text))
 
-        # 线程锁
-        self.lock = threading.Lock()
-
-        # 类变量
-        CacheItem.cache = {} if not hasattr(CacheItem, "cache") else CacheItem.cache
-
-    def __repr__(self) -> str:
-        return (
-            f"{type(self).__name__}({self.get_vars()})"
-        )
-
-    def get_vars(self) -> dict:
-        return {
-            k:v
-            for k, v in vars(self).items()
-            if isinstance(v, CacheItem.TYPE_FILTER) 
-        }
-
-    # 获取行号
-    def get_row_index(self) -> int:
-        with self.lock:
-            return self.row_index
-
-    # 设置行号
-    def set_row_index(self, row_index: int) -> None:
-        with self.lock:
-            self.row_index = row_index
-
-    # 获取文本序号
-    def get_text_index(self) -> int:
-        with self.lock:
-            return self.text_index
-
-    # 设置文本序号
-    def set_text_index(self, text_index: int) -> None:
-        with self.lock:
-            self.text_index = text_index
-
-    # 获取翻译状态
-    def get_translation_status(self) -> int:
-        with self.lock:
-            return self.translation_status
-
-    # 设置翻译状态
-    def set_translation_status(self, translation_status: int) -> None:
-        with self.lock:
-            self.translation_status = translation_status
-
-    # 获取翻译模型
-    def get_model(self) -> str:
-        with self.lock:
-            return self.model
-
-    # 设置翻译模型
-    def set_model(self, model: str) -> None:
-        with self.lock:
-            self.model = model
-
-    # 获取原文
-    def get_source_text(self) -> str:
-        with self.lock:
-            return self.source_text
-
-    # 设置原文
-    def set_source_text(self, source_text: str) -> None:
-        with self.lock:
-            self.source_text = source_text
-
-    # 获取译文
-    def get_translated_text(self) -> str:
-        with self.lock:
-            return self.translated_text
-
-    # 设置译文
-    def set_translated_text(self, translated_text: str) -> None:
-        with self.lock:
-            # 有时候模型的回复反序列化以后会是 int 等非字符类型，所以这里要强制转换成字符串
-            # TODO:可能需要更好的处理方式
-            if isinstance(translated_text, (int, float)):
-                self.translated_text = str(translated_text)
-            else:
-                self.translated_text = translated_text
-
-    # 获取文件名
-    def get_file_name(self) -> str:
-        with self.lock:
-            return self.file_name
-
-    # 设置文件名
-    def set_file_name(self, file_name: str) -> None:
-        with self.lock:
-            self.file_name = file_name
-
-    # 获取文件路径
-    def get_storage_path(self) -> str:
-        with self.lock:
-            return self.storage_path
-
-    # 设置文件路径
-    def set_storage_path(self, storage_path: str) -> None:
-        with self.lock:
-            self.storage_path = storage_path
-
-    # 获取文件项目类型
-    def get_file_project_type(self) -> str:
-        with self.lock:
-            return self.file_project_type
-
-    # 设置文件项目类型
-    def set_file_project_type(self, file_project_type) -> None:
-        with self.lock:
-            self.file_project_type = file_project_type
-
-    # 获取 Token 数量
-    def get_token_count(self) -> int:
-        with self.lock:
-            if self.source_text not in CacheItem.cache:
-                CacheItem.cache[self.source_text] = len(tiktoken.get_encoding("cl100k_base").encode(self.source_text))
-
-            return CacheItem.cache[self.source_text]
+    STATUS: ClassVar[Status] = Status
