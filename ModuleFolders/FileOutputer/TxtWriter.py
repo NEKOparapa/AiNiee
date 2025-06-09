4733e14e2d621cbe57457fe786f93654ba55ac0e
nekomiya-hinata <46987091+nekomiya-hinata@users.noreply.github.com>
2025-06-09 23:44:31 +0800

    perf: 去除原来txt读写器中的空白字符保留逻辑，统一使用文本处理器

--- ModuleFolders/FileOutputer/TxtWriter.py
+++ ModuleFolders/FileOutputer/TxtWriter.py
@@ -47,17 +47,18 @@ class TxtWriter(BaseBilingualWriter, BaseTranslatedWriter):
     def _item_to_bilingual_line(self, item: CacheItem):
         # 至少2个换行，让双语排版不那么紧凑，但用户说不需要，要原文排版
         line_break = "\n" * max(item.require_extra("line_break") + 1, 1)
-        indent = item.require_extra("sentence_indent")
+        _indent = item.require_extra("sentence_indent")
 
         return (
-            f"{indent}{item.source_text.lstrip()}\n"
-            f"{indent}{item.translated_text.lstrip()}{line_break}"
+            f"{item.source_text}\n"
+            f"{item.translated_text}{line_break}"
         )
 
     def _item_to_translated_line(self, item: CacheItem):
         line_break = "\n" * (item.require_extra("line_break") + 1)
 
-        return f"{item.require_extra("sentence_indent")}{item.translated_text.lstrip()}{line_break}"
+        # 原来有{item.require_extra("sentence_indent")}与translated_text的.lstrip()，现在不需要了
+        return f"{item.translated_text}{line_break}"
 
     @classmethod
     def get_project_type(self):
