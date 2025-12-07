d9c9a4abb83a9b3409615c3d4fbb7d5365ca0116
nekomiya-hinata <46987091+nekomiya-hinata@users.noreply.github.com>
2025-12-07 17:51:06 +0800

    🐛 fix(`ModuleFolders`): 修复 `os.environ` 补丁方法签名并更新 tiktoken 编码文件大小

--- ModuleFolders/TiktokenLoader.py
+++ ModuleFolders/TiktokenLoader.py
@@ -94,11 +94,11 @@ def initialize_tiktoken():
         required_encodings = {
             "o200k_base": {
                 "url": "https://openaipublic.blob.core.windows.net/encodings/o200k_base.tiktoken",
-                "size": 3613922,
+                "size": 3813920,
             },
             "cl100k_base": {
                 "url": "https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken",
-                "size": 1681126,
+                "size": 1781382,
             }
         }
 
